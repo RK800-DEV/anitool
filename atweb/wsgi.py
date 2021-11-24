ce7707f66004b9d3f574bb4ed7ada0493476fa02
RK800 <85429713+RK800-DEV@users.noreply.github.com>
2021-11-25 02:23:01 +0600

    Update wsgi.py

--- atweb/wsgi.py
+++ atweb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'at_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atweb.settings')
 
 application = get_wsgi_application()
